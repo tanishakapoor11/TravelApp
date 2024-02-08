5375796254d5b1328c72137b1e08928bc5993bdb
Tanisha Kapoor <tanishakapoor2020@gmail.com>
2024-02-08 16:40:06 +0530

    Hero section complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,6 +20,8 @@ const Navbar = () => {
         <div className="lg:flexCenter hidden">
             <Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green" />
         </div>
+
+        <Image src="menu.svg" alt="menu" width={32} height={32} className="inline-block cursor-pointer lg:hidden" />
     </nav>
   )
 }
